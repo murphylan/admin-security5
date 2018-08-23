d91d94eb92eaba2d36ae6b5798fed42730471eef
zejun.lan <murphylan@hotmail.com>
2018-08-23 19:42:33 +0800

    add mp

--- src/test/java/com/udream/wechat/admin/WeixinApplicationTests.java
+++ src/test/java/com/udream/wechat/admin/WeixinApplicationTests.java
@@ -5,12 +5,13 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
 @SpringBootTest
-public class AdminApplicationTests {
+public class WeixinApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		System.out.println("test");
 	}
 
 }
